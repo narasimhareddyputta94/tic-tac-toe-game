01c4004c8860944525c3cf9686f9afddecdcc883
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-06-08 00:31:45 -0500

    implementing all required models for tic tac toe game design

--- src/main/java/models/Cell.java
+++ src/main/java/models/Cell.java
@@ -6,25 +6,5 @@ public class Cell {
     private int column;
     private char symbol;
 
-    public Cell(int row, int column) {
-        this.row = row;
-        this.column = column;
-        this.symbol = ' ';
-    }
 
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-
-    public char getSymbol() {
-        return symbol;
-    }
-
-    public void setSymbol(char symbol) {
-        this.symbol = symbol;
-    }
 }
