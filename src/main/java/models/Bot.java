15f22cbdd7ae64a614eb0b72da4dbfed935820e6
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-06-10 18:44:41 -0500

    done

--- src/main/java/models/Bot.java
+++ src/main/java/models/Bot.java
@@ -7,7 +7,11 @@ import botplayingstrategy.*;
 public class Bot extends Player{
    private BotDifficultylevel botDifficultylevel;
    private BotPlayingStrategy botPlayingStrategy;
-
+    public Bot(char symbol, String name, int id, PlayerType playerType,BotDifficultylevel botDifficultylevel) {
+        super(symbol, name, id, playerType);
+        this.botDifficultylevel = botDifficultylevel;
+        this.botPlayingStrategy = BotPlayingStrategyFactory.getBotPlayingStrategyforDifficultylevel(botDifficultylevel);
+    }
 
     public Cell makeMove(Board board) {
         Cell cell =  botPlayingStrategy.makeMove(board);
